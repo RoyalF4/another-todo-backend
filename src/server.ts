dc7259c042a218af129ac4a49385fddf31ac86c1
Royal Finch <Royal2791@gmail.com>
2024-06-07 12:17:52 -0400

    feat: :sparkles: connect to database

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,22 @@
-import dotenv from 'dotenv/config';
-import express from 'express';
+import 'dotenv/config';
+import mongoose from 'mongoose';
 import { app } from './app';
 
+// connect to db
+const dbUrl = process.env.DATABASE;
+const dbPassword = process.env.DATABASE_PASSWORD;
+
+if (!dbUrl || !dbPassword) {
+  throw new Error(
+    'DATABASE AND DATABASE_PASSWORD environment variables are required'
+  );
+}
+const DB = dbUrl.replace('<password>', dbPassword);
+mongoose.connect(DB, {}).then(() => {
+  console.log('DB connection successful!');
+});
+
+// get port
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
